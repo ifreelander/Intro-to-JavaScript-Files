1ac4310c3820df14b87a1ded40e5e2a5da762263
ifreelander <roya.taleghani@gmail.com>
2017-03-26 16:33:25 +1100

    last value in the array

---
+++ w2/arrayLastValue.js
@@ -0,0 +1,10 @@
+var arr = ['bob','tim','alexa']
+
+//how do you find out the last value of the array?
+// It's at index length -1
+console.log(arr[arr.length-1]);
+
+//Print alexa by using for loop
+ //for (var i=0 ; i < arr.length ; i++){
+  //console.log(arr[arr.length-1])
+// }
